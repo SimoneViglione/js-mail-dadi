d9626802d5d0b6bf3571e2a456f9ae2a3ce9e05b
Simone Viglione <zimoviglio51@gmail.com>
2023-02-27 16:44:32 +0100

    esercizio email finito + bonus

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,18 +1,47 @@
-const userEmail = prompt("Inserisci la tua email");
-
 const emailArray = [ "tonycannavacciuolo@gmail.com", "brunobarbie@hotmail.com", "quellaltrononmiricordoilnome@libero.it" ];
+
+/*const userEmail = prompt("Inserisci la tua email");
+
+let emailVerificata = false;
 
 for (let i = 0; i < emailArray.length; i++) {
     if (userEmail === emailArray[i]) {
-        alert("Benvenuto " + userEmail + "!");
-    }
-    else {
-        alert("Ci dispiace, Non hai i permessi per accedere a questa pagina :( ");
+        emailVerificata = true;
     }
 }
 
+if  (emailVerificata) {
+    alert("Benvenuto " + userEmail + "!");
+} else {
+    alert("Ci dispiace, Non hai i permessi per accedere a questa pagina :( ");
+}
+*/
 
 
+
+//Bonus 
+
+
+const verify = document.querySelector('#verify');
+
+verify.addEventListener('click', verify_email); 
+    function verify_email() {
+        const userEmail = document.querySelector('#email').value;
+
+        let emailVerificata = false;
+
+        for (let i = 0; i < emailArray.length; i++) {
+            if (userEmail === emailArray[i]) {
+                emailVerificata = true;
+            }
+        }
+
+        if  (emailVerificata) {
+            alert("Benvenuto " + userEmail + "!");
+        } else {
+            alert("Ci dispiace, Non hai i permessi per accedere a questa pagina :( ");
+        }
+}
 
 
 
